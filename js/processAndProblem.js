59d4e5ff2a989188b9d20deefa0f9716fce42563
ginnychen19 <71680206+ginnychen19@users.noreply.github.com>
2022-12-21 02:45:52 +0800

    繼續送~

--- js/processAndProblem.js
+++ js/processAndProblem.js
@@ -1,39 +1,37 @@
-//判斷是否到process和problem區，到了再執行進入動畫
-//加入 class="animate__animated animate__fadeInUp"
+
 window.addEventListener('load', () => {
+
+    /*1.判斷是否到process和problem區，到了再執行進入動畫
+      加入 class="animate__animated animate__fadeInUp"
+    */
     const process = document.querySelector('#process');
-const problem = document.querySelector('#problem');
+    const problem = document.querySelector('#problem');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            const element = entry.target;
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        const element = entry.target;
+            if (entry.isIntersecting) {
+                setTimeout(() => {
+                    element.classList.add('animate__fadeInUp');
+                }, 200);
+            } else { }
+        });
 
-        if (entry.isIntersecting) {
-            setTimeout(() => {
-                element.style.display = "flex";
-                element.classList.add('animate__animated', 'animate__fadeInUp');
-            }, 200);
-        } else {
-            
-        }
+        observer.observe(process);
+        observer.observe(problem);
     });
-});
 
-observer.observe(process);
-observer.observe(problem);
-
-$(function () {
+    /*2.判斷是否到process和problem區，到了再執行進入動畫
+      加入 class="animate__animated animate__fadeInUp"
+    */
     //全部選擇隱藏
     $('div[id^="Q_"]').hide();
     $('#sltQ').change(function () {
         let sltValue = $(this).val();
-        console.log(sltValue);
-
+        //console.log(sltValue);
         $('div[id^="Q_"]').hide();
         //指定選擇顯示
         $(sltValue).show();
     });
-});
 
 });
-    
